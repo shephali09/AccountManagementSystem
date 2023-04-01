9a5a3735c92a2f87492c6110f9f1c837be5b7f0d
shephali09 <hiremathshephali9@gmail.com>
2023-04-01 08:04:56 +0530

    Add: Print welcome message

---
+++ src/com/bridgelabz/accountmanagementsystem/AccountManagementMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.accountmanagementsystem;
+
+public class AccountManagementMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Account Management System!!");
+
+	}
+
+}
